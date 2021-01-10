a5d3b639eff832faf4b4a10f4156c82dad6e2cb0
Ben Bullock <benkasminbullock@gmail.com>
2021-01-10 11:47:38 +0900

    HTML tags, classes; moon example; to-do comments
    Version to 0.00_02.
    
    Move git.t to zgit.t

--- lib/HTML/Make/Calendar.pm
+++ lib/HTML/Make/Calendar.pm
@@ -9,14 +9,62 @@ our @EXPORT_OK = qw/calendar/;
 our %EXPORT_TAGS = (
     all => \@EXPORT_OK,
 );
-our $VERSION = '0.00_01';
+our $VERSION = '0.00_02';
 
 use Date::Calc ':all';
 use HTML::Make;
 
+# Default HTML elements and classes.
+
+# To do: Put this in a configuration file.
+
+my %html = (
+    calendar => {
+	element => 'table',
+	class => '',
+	desc => 'the calendar itself',
+    },
+    week => {
+	element => 'tr',
+	class => 'cal-week',
+	desc => 'a week',
+    },
+    day => {
+	element => 'td',
+	class => 'cal-day',
+	desc => 'a day',
+    },
+    dow => {
+	element => 'th',
+	class => 'cal-dow',
+	desc => 'the day of the week (Monday, Tuesday, etc.)',
+    },
+);
+
+# Add an HTML element defined by $thing to $parent.
+
+sub add_el
+{
+    my ($parent, $thing) = @_;
+    my $class = $thing->{class};
+    my $type = $thing->{element};
+    my $element;
+    if ($class) {
+	# HTML::Make should have a class pusher since it is so common
+	# http://mikan/bugs/bug/2108
+	$element = $parent->push ($type, attr => {class => $class});
+    }
+    else {
+	# Allow non-class elements if the user doesn't want a class.
+	$element = $parent->push ($type);
+    }
+    return $element;
+}
+
 sub calendar
 {
     my (%options) = @_;
+    # To do: Reduce repetitiveness.
     my $verbose;
     if ($options{verbose}) {
 	$verbose = 1;
@@ -41,6 +89,8 @@ sub calendar
 	$cdata = $options{cdata};
 	delete $options{cdata};
     }
+    # To do: Allow the user to use their own HTML tags.
+
     for my $k (sort keys %options) {
 	if ($options{$k}) {
 	    carp "Unknown option '$k'";
@@ -49,17 +99,19 @@ sub calendar
     }
     my $dim = Days_in_Month ($year, $month);
     if ($verbose) {
+	# To do: Add a messaging routine with caller line numbers
+	# rather than just use print.
 	print "There are $dim days in month $month of $year.\n";
     }
     my @dow;
-    # The number of rows (weeks)
-    my $rows = 1;
+    # The number of weeks
+    my $weeks = 1;
     my $prev = 0;
     for my $day (1..$dim) {
 	my $dow = Day_of_Week ($year, $month, $day);
 	$dow[$day] = $dow;
 	if ($dow == 1 || $dow < $prev) {
-	    $rows++;
+	    $weeks++;
 	}
 	$prev = $dow;
     }
@@ -67,9 +119,11 @@ sub calendar
     my $fill_start = $dow[1] - 1;
     my $fill_end = 7 - $dow[-1];
     if ($verbose) {
-	print "Start $fill_start, end $fill_end, rows $rows\n";
+	print "Start $fill_start, end $fill_end, weeks $weeks\n";
     }
     my @cells;
+    # To do: Allow the user to colour or otherwise alter empty cells,
+    # for example with a callback or with a user-defined class.
     for (1..$fill_start) {
 	push @cells, {};
     }
@@ -79,25 +133,37 @@ sub calendar
     for (1..$fill_end) {
 	push @cells, {};
     }
-    my $table = HTML::Make->new ('table');
-    # This is the correct HTML, although nobody really does this.
-    my $tbody = $table->push ('tbody');
+    my $calendar = HTML::Make->new ($html{calendar});
+    # As far as I know, <table><tbody> is the correct HTML, although
+    # nobody really does this.
 
+    # To do: inspect the type of $html{calendar} and don't add the
+    # <tbody> unless it is a <table> element.
+    my $tbody = $calendar->push ('tbody');
+    # To do: These should be overridden if the caller doesn't want to
+    # use table, tr, td to construct the calendar.
     my $titler = $tbody->push ('tr');
     my $titleh = $titler->push ('th', attr => {colspan => 7});
+    # To do: Allow the caller to override this.
     my $my = Month_to_Text ($month) . " $year";
     $titleh->add_text ($my);
-
+    # To do: Allow the user to override this.
     my $wdr = $tbody->push ('tr');
-    for my $wd (1..7) {
-	my $wdt = substr (Day_of_Week_to_Text ($wd), 0, 2);
-	$wdr->push ('th', text => $wdt);
+    for my $dow (1..7) {
+	# To do: Allow the user to use their own weekdays (possibly
+	# allow them to use the language specifier of Date::Calc).
+	my $wdt = substr (Day_of_Week_to_Text ($dow), 0, 2);
+	my $dow_el = add_el ($wdr, $html{dow});
+	$dow_el->add_text ($wdt);
     }
-    for my $wom (1..$rows) {
-	my $tr = $tbody->push ('tr', attr => {class => 'cal-row'});
+    # wom = week of month
+    for my $wom (1..$weeks) {
+	my $week = add_el ($tbody, $html{week});
+	# dow = day of week
 	for my $dow (1..7) {
-	    my $td = $tr->push ('td', attr => {class => 'cal-day'});
+	    my $day = add_el ($week, $html{day});
 	    my $cell = shift @cells;
+	    # dom = day of month
 	    my $dom = $cell->{dom};
 	    if (defined $dom) {
 		if ($dayc) {
@@ -109,16 +175,17 @@ sub calendar
 			      dow => $dow,
 			      wom => $wom,
 			  }, 
-			  $td);
+			  $day);
 		}
 		else {
-		    $td->push ('span', text => $dom,
-			       attr => {class => 'cal-dom'});
+		    $day->push ('span', text => $dom,
+				attr => {class => 'cal-dom'});
 		}
 	    }
+	    # To do: allow a callback on the packing cells
 	}
     }
-    return $table;
+    return $calendar;
 }
 
 1;
